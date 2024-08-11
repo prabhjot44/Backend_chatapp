950def270f223a1b725b88f1284906ad95e7f1e1
prabhjot44 <iamprabh0001@gmail.com>
2024-08-11 21:27:24 +0530

    .

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000" || "https://frontend-chatapp-xi.vercel.app/" ,
+    origin: "http://localhost:3000" || "https://frontend-chatapp-xi.vercel.app" ,
     methods: ["GET", "POST"],
   },
 });
